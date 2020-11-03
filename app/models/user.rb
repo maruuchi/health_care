7781ffee1089cab1c14e77f9a328c48a14372b14
soma-maruuchi <mwa60346@gmail.com>
2020-11-03 21:51:31 +0900

    テストコードの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  # classメソッドなのでselfをつける
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe '文字数の検証' do
+    
+    it 'メールアドレスが255文字以内の場合、有効であること' do
+      @user.email = 'a' * 243 + '@example.com'
+      expect(@user).to be_valid
+    end
+    
+    it 'メールアドレスが256文字以上の場合、無効であること' do
+      @user.email = 'a' * 244 + '@example.com'
+      expect(@user).not_to be_valid
+    end
+    
+    it 'パスワードが8文字以上の場合、有効であること' do
+      @user.password = 'a' * 8
+      expect(@user).to be_valid
+    end
+    
+    it 'パスワードが7文字以下の場合、無効であること' do
+      @user.password = 'a' * 7
+      expect(@user).not_to be_valid
+    end
+  end
+end
