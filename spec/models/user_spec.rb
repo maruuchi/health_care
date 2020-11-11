64a6b38cadf340ea1fd96b2c4e57032388f831e2
soma-maruuchi <mwa60346@gmail.com>
2020-11-11 22:22:08 +0900

    テストコードの変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,23 +5,25 @@ RSpec.describe User, type: :model do
   describe '文字数の検証' do
 
     it 'メールアドレスが255文字以内の場合、有効であること' do
-      @user.email = 'a' * 243 + '@example.com'
-      expect(@user).to be_valid
+      user = User.new(email: "a" * 255, password: "password")
+      expect(user).to be_valid
     end
     
-    it 'メールアドレスが256文字以上の場合、無効であること' do
-      @user.email = 'a' * 244 + '@example.com'
-      expect(@user).not_to be_valid
+    it "ユーザー名が256文字以上の場合は登録できない" do
+      user = User.new(email: "a" * 256, password: "password")
+      expect(user).not_to be_valid 
     end
     
     it 'パスワードが8文字以上の場合、有効であること' do
-      @user.password = 'a' * 8
+      password = 'a' * 8
       expect(@user).to be_valid
     end
     
-    it 'パスワードが7文字以下の場合、無効であること' do
-      @user.password = 'a' * 7
-      expect(@user).not_to be_valid
+    
+    it "パスワードが7文字以下の場合は登録できない" do
+      user = User.new(email: "email", password: "a" * 7 )
+      expect(user.invalid?).to be true
     end
+    
   end
 end
