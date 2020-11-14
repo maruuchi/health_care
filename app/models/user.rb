b35ef934c5c83443a64846f0c05bbcbe0d945c00
soma-maruuchi <mwa60346@gmail.com>
2020-11-14 10:19:11 +0900

    パスワードのテストコード変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ApplicationRecord
 
   
   validates :email, presence: true, 
-                    length: { maximum: 255 } 
+                    length: { maximum: 255 } ,
+                    allow_nil: true
                     
 
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true                  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe User, type: :model do
   describe '文字数の検証' do
 
     it 'メールアドレスが255文字以内の場合、有効であること' do
-      user = User.new(email: "a" * 243 + '@example.com', password: "password")
+      user = User.new(email: "a" * 243 + "@example.com", password: "password")
       expect(user).to be_valid
     end
     
     it 'メールアドレスが256文字以上の場合、無効であること' do
-      user = User.new(email: "a" * 244 + '@example.com', password: "password")
+      user = User.new(email: "a" * 244 + "@example.com", password: "password")
       expect(user).not_to be_valid 
     end
     
     it 'パスワードが8文字以上の場合、有効であること' do
-      password = 'a' * 8
-      expect(@user).to be_valid
+      user = User.new(email: "email", password: "a" * 8 )
+      expect(user).to be_valid
     end
     
     
